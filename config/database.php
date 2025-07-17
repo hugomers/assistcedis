2ea00035944ddd1acd2d3a11bcc5d958584237f2
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-07-16 18:07:39 -0600

    se configura timezone

--- config/database.php
+++ config/database.php
@@ -82,6 +82,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'timezone'  => env('DB_TIMEZONE', '+00:00'),
         ],
 
         'pgsql' => [
