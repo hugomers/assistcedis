b852c767f4e9304a000dca4a4be453e063b33fb2
HUGO MUÑOZ <hugomo0897@gmail.com>
2025-12-21 15:40:07 -0600

    se agrega

--- app/Models/ProductOrderedPivot.php
+++ app/Models/ProductOrderedPivot.php
@@ -9,7 +9,11 @@ class ProductOrderedPivot extends Pivot
 {
     protected $connection = 'vizapi';
     protected $table = "product_ordered";
-    protected $guarded = [];
+
+    public $incrementing = false;
+    protected $primaryKey = null;
+
+    protected $fillable  = ['kit', 'units', 'price', '_price_list', "comments", "total", "amount", '_supply_by', 'toDelivered', 'ipack', 'amountDelivered'];
 
    public function supplyBy()
    {
